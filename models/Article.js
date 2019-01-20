ea16a8f2a94e2508d4a2da08a7527e57e6e22451
sujithjohn <johnsujith2000@gmail.com>
2019-01-20 07:58:50 -0500

    NYT Scraper

--- models/Article.js
+++ models/Article.js
@@ -34,14 +34,9 @@ var ArticleSchema = new Schema({
     required: false,
     unique: false
   },
-  // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
-  note: {
-    type: [{ type: Schema.Types.ObjectId, ref: 'Note'}],
-  }
+  
 });
 
 var Article = mongoose.model("Article", ArticleSchema);
 
-module.exports = Article; //exporting Article
+module.exports = Article; 
